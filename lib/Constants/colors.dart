430311aaba8a69f9b56ff72878b378a6440c88f8
Dawood <dawoodqurashi448@gmail.com>
2023-12-31 13:40:12 +0500

    intial Setup

--- lib/Constants/colors.dart
+++ lib/Constants/colors.dart
@@ -5,14 +5,15 @@ class AppColors {
 
   //common
   static const Color white = Color(0xFFffffff);
-  static const Color background = Color(0xFFffffff);
+  static const Color background = Color(0xFFF5F9FF);
   static const Color backgroundAccent = Color(0xFFC3C3C3);
   static const Color transparent = Color(0x00000000);
   static const Color green = Color(0xFF27AE60);
   static const Color lightG = Color(0xFFF0F0F0);
   static const Color black = Color(0xFF000000);
   static const Color grey = Color(0xFF747474);
-  static const Color hintT = Color(0xFFAAAAAA);
-  static const Color outlineB = Color.fromARGB(255, 207, 206, 206);
+  static const Color hintT = Color(0xFF222427);
+  static const Color outlineB = Color(0xFFD3D6E0);
   static const Color error = Color(0xFFFF0000);
+  static const Color labelT = Color(0xFFBCBFC3);
 }
--- lib/Constants/theme.dart
+++ lib/Constants/theme.dart
@@ -5,13 +5,14 @@ import 'colors.dart';
 class AppTheme {
   static ThemeData lightTheme(BuildContext context) {
     return ThemeData(
+      scaffoldBackgroundColor: AppColors.background,
       useMaterial3: true,
       colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
       filledButtonTheme: FilledButtonThemeData(
         style: FilledButton.styleFrom(
           backgroundColor: AppColors.primary,
           elevation: 2,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
           textStyle: Theme.of(context)
               .textTheme
@@ -22,11 +23,11 @@ class AppTheme {
       ),
       outlinedButtonTheme: OutlinedButtonThemeData(
         style: OutlinedButton.styleFrom(
-          side: const BorderSide(color: AppColors.outlineB),
-          backgroundColor: AppColors.outlineB,
-          foregroundColor: AppColors.black,
+          side: const BorderSide(color: AppColors.primary),
+          backgroundColor: AppColors.background,
+          foregroundColor: AppColors.white,
           elevation: 2,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
           textStyle: Theme.of(context).textTheme.labelLarge!.copyWith(
               color: AppColors.black, fontWeight: FontWeight.w600, ),
@@ -36,15 +37,16 @@ class AppTheme {
       inputDecorationTheme: InputDecorationTheme(
         border: OutlineInputBorder(
           borderSide: const BorderSide(color: AppColors.outlineB),
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(8),
+
         ),
         focusedBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: AppColors.outlineB),
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(8),
         ),
         enabledBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: AppColors.outlineB),
-          borderRadius: BorderRadius.circular(30),
+          borderRadius: BorderRadius.circular(8),
         ),
         contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
       ),
