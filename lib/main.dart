d1b2046c3cc1165f290685c24348fd8753642392
Dawood <dawoodqurashi448@gmail.com>
2024-01-02 00:39:34 +0500

    google auth final

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'Constants/theme.dart';
+import 'Data/Local/hive_storage.dart';
 import 'View/Splash/splash_screen.dart';
 import 'View/auth_view/login_screen.dart';
 import 'View/auth_view/signup_screen.dart';
@@ -9,6 +10,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await LocalStorage.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'TracCar',
       theme: AppTheme.lightTheme(context),
-      home:  SignUpScreen(),
+      home: SignUpScreen(),
     );
   }
 }
